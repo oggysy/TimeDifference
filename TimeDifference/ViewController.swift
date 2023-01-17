848c164e86b29bebae0f90b0697b5652816d61e2
oggysy <ogisoys@gmail.com>
2023-01-18 08:26:00 +0900

    first commit

--- TimeDifference/ViewController.swift
+++ TimeDifference/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentTime: UILabel!
+    @IBOutlet weak var pressedButtonTime: UILabel!
+    @IBOutlet weak var differentTime: UILabel!
+    var currentTimeData = Date()
+    var pressedButtonTimeData:Date? = nil
+    var differentTimeData:Int? = nil
+    let dateFormatter = DateFormatter()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMdHms", options: 0, locale: Locale(identifier: "ja_JP"))
+        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        }
+    
+    
+    @objc func updateTimer(){
+        currentTimeData = Date()
+        currentTime.text = dateFormatter.string(from: currentTimeData)
+        if pressedButtonTimeData != nil {
+            differentTimeData = Int(currentTimeData.timeIntervalSince(pressedButtonTimeData!))
+            differentTime.text = String(differentTimeData!)
+        }
     }
-
+    
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        pressedButtonTimeData = Date()
+        pressedButtonTime.text = dateFormatter.string(from: pressedButtonTimeData!)
+    }
+    
+    
 
 }
 
